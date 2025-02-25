fb9f0d215922c35c94f66c18a0e6539b04c94bf0
sumaiai <sumaiaislam568@gmail.com>
2025-02-25 01:12:43 -0500

    added buttons

---
+++ map.js
@@ -0,0 +1,403 @@
+var simplemaps_usmap_mapdata = {
+    main_settings: {
+        //General settings
+        width: "650", //'700' or 'responsive'
+        background_color: "#FFFFFF",
+        background_transparent: "yes",
+        label_color: "",
+        hide_labels: "no",
+        hide_eastern_labels: "yes",
+        border_color: "#FFFFFF",
+
+        //State defaults
+        state_description: "Navigate to state",
+        state_color: "#88A4BC",
+        state_hover_color: "#3B729F",
+        state_url: "",
+        all_states_inactive: "no",
+
+        //Location defaults
+        location_description: "Location description",
+        location_color: "#FF0067",
+        location_opacity: "0.8",
+        location_url: "",
+        location_size: "20",
+        location_type: "circle",
+        all_locations_inactive: "no",
+        url_new_tab: "yes",
+        auto_load: "yes",
+
+        //Zoom settings
+        zoom: "yes",
+        initial_zoom: "-1",
+        initial_zoom_solo: "no",
+        popups: "detect",
+        state_image_url: "",
+        state_image_position: "",
+        all_states_zoomable: "",
+        location_image_url: "",
+        label_hover_color: "",
+        label_size: "",
+        label_font: "",
+        div: "map"
+    },
+    state_specific: {
+        HI: {
+            name: "Hawaii",
+            url: "https://simplemaps.com",
+            description: "Navigate to state"
+        },
+        AK: {
+            name: "Alaska",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        FL: {
+            name: "Florida",
+            inactive: "no",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        NH: {
+            name: "New Hampshire",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        VT: {
+            name: "Vermont",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        ME: {
+            name: "Maine",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        RI: {
+            name: "Rhode Island",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        NY: {
+            name: "New York",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        PA: {
+            name: "Pennsylvania",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        NJ: {
+            name: "New Jersey",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        DE: {
+            name: "Delaware",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        MD: {
+            name: "Maryland",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        VA: {
+            name: "Virginia",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        WV: {
+            name: "West Virginia",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        OH: {
+            name: "Ohio",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        IN: {
+            name: "Indiana",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        IL: {
+            name: "Illinois",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        CT: {
+            name: "Connecticut",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        WI: {
+            name: "Wisconsin",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        NC: {
+            name: "North Carolina",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        DC: {
+            name: "District of Columbia",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        MA: {
+            name: "Massachusetts",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        TN: {
+            name: "Tennessee",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        AR: {
+            name: "Arkansas",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        MO: {
+            name: "Missouri",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        GA: {
+            name: "Georgia",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        SC: {
+            name: "South Carolina",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        KY: {
+            name: "Kentucky",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        AL: {
+            name: "Alabama",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        LA: {
+            name: "Louisiana",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        MS: {
+            name: "Mississippi",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        IA: {
+            name: "Iowa",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        MN: {
+            name: "Minnesota",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        OK: {
+            name: "Oklahoma",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        TX: {
+            name: "Texas",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        NM: {
+            name: "New Mexico",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        KS: {
+            name: "Kansas",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        NE: {
+            name: "Nebraska",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        SD: {
+            name: "South Dakota",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        ND: {
+            name: "North Dakota",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        WY: {
+            name: "Wyoming",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        MT: {
+            name: "Montana",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        CO: {
+            name: "Colorado",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        UT: {
+            name: "Utah",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        AZ: {
+            name: "Arizona",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        NV: {
+            name: "Nevada",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        OR: {
+            name: "Oregon",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        WA: {
+            name: "Washington",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        CA: {
+            name: "California",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        MI: {
+            name: "Michigan",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        ID: {
+            name: "Idaho",
+            description: "Navigate to state",
+            url: "https://simplemaps.com"
+        },
+        GU: {
+            name: "Guam",
+            hide: "yes"
+        },
+        VI: {
+            name: "Virgin Islands",
+            hide: "yes"
+        },
+        PR: {
+            name: "Puerto Rico",
+            hide: "yes"
+        },
+        MP: {
+            name: "Northern Mariana Islands",
+            hide: "yes"
+        }
+    },
+    locations: {},
+    regions: {
+        "0": {
+            name: "West",
+            states: [
+                "ID",
+                "MT",
+                "WY",
+                "NV",
+                "UT",
+                "CO",
+                "AZ",
+                "NM",
+                "AK",
+                "WA",
+                "OR",
+                "CA",
+                "HI"
+            ],
+            color: "#459676",
+            cascade: "yes",
+            hover_color: "#837C6C",
+            description: "Zoomable region"
+        },
+        "1": {
+            name: "Midwest",
+            states: [
+                "IL",
+                "IN",
+                "IA",
+                "MI",
+                "KY",
+                "ND",
+                "SD",
+                "MN",
+                "WI",
+                "MO",
+                "OH",
+                "KS",
+                "NE"
+            ],
+            color: "#4D5C25",
+            cascade: "yes",
+            hover_color: "#837C6C",
+            description: "Clickable region",
+            url: "https://simplemaps.com"
+        },
+        "2": {
+            name: "South",
+            states: [
+                "DE",
+                "MD",
+                "DC",
+                "VA",
+                "WV",
+                "NC",
+                "SC",
+                "GA",
+                "FL",
+                "TN",
+                "MS",
+                "AL",
+                "OK",
+                "TX",
+                "AR",
+                "LA"
+            ],
+            color: "#174E31",
+            cascade: "yes",
+            hover_color: "#837C6C",
+            description: "Zoomable region"
+        },
+        "3": {
+            name: "Northeast",
+            states: [
+                "ME",
+                "NH",
+                "VT",
+                "MA",
+                "RI",
+                "CT",
+                "NY",
+                "PA",
+                "NJ"
+            ],
+            color: "#37734D",
